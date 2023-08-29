a004b81fb8587daae2bc1ccd5cd5702cffd8c1f9
Cyberflixt <cyberflixt@gmail.com>
2023-08-29 17:28:04 +0200

    merge local

---
+++ public/storage0/scripts.js
@@ -0,0 +1,9 @@
+
+divCon = document.querySelector('#divCon');
+inputData = document.querySelector('#inputData');
+btnData = document.querySelector('#btnData');
+
+btnData.addEventListener('click', (e) => {
+	console.log(0);
+	inputData.value = "";
+}, false)
