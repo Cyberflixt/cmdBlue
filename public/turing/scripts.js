3f5b4cd0e79254a21a60a00743e4c7a0b953b48f
Cyberflixt <54700008+Cyberflixt@users.noreply.github.com>
2024-05-17 17:06:46 +0200

    js

--- public/turing/scripts.js
+++ public/turing/scripts.js
@@ -8,9 +8,42 @@ const elemControlPrevious = document.getElementById("elemControlPrevious");
 const elemControlPause = document.getElementById("elemControlPause");
 const elemControlPlay = document.getElementById("elemControlPlay");
 
-var turingRules = {};
+const elemDataDefault = document.getElementById("elemDataDefault");
+const elemDataCurrent = document.getElementById("elemDataCurrent");
+const elemDataCopy = document.getElementById("elemDataCopy");
 
-function buildRuleElement(){
+var turingRules = {};
+var currentData = [];
+var defaultData = [];
+var currentPos = 0;
+var currentState = 0;
+var defaultPos = 0;
+
+var playing = false;
+var lastStep = 0;
+var stepCool = 1000;
+
+
+function saveRules(){
+    localStorage.setItem("rules", JSON.stringify(turingRules));
+}
+function retrieveRules(){
+    const save = localStorage.getItem("rules");
+    if (save){
+        turingRules = JSON.parse(save);
+        rebuildRules();
+    }
+}
+function rebuildRules(){
+    console.log(turingRules);
+    Object.keys(turingRules).map((k) => {
+        const v = turingRules[k];
+        const ks = k.split(";");
+        buildRuleElement(ks[0], ks[1], v[0], v[1], v[2]);
+    });
+}
+
+function buildRuleElement(a,b,c,d,e){
     const inner = `
     <div class="flexx">
         <input type="text" placeholder="cell"  id="inputCell"></input>
@@ -37,11 +70,18 @@ function buildRuleElement(){
     const outputState = child.querySelector("#outputState");
     const outputHeading = child.querySelector("#outputHeading");
 
+    inputCell.value = a || '';
+    inputState.value = b || '';
+    outputCell.value = c || '';
+    outputState.value = d || '';
+    outputHeading.value = e || '';
+
     let oldKey;
 
     elemClose.addEventListener("click", () => {
         elemRulesHolder.removeChild(child);
         delete turingRules[oldKey];
+        saveRules();
     });
 
     function ruleChanged(){
@@ -51,7 +91,7 @@ function buildRuleElement(){
         const v = [outputCell.value, outputState.value, outputHeading.value];
 
         turingRules[oldKey] = v;
-        console.log(turingRules);
+        saveRules();
     }
 
     inputCell.addEventListener("input", ruleChanged);
@@ -59,6 +99,7 @@ function buildRuleElement(){
     outputCell.addEventListener("input", ruleChanged);
     outputState.addEventListener("input", ruleChanged);
     outputHeading.addEventListener("input", ruleChanged);
+    ruleChanged();
 
     elemRulesHolder.appendChild(child);
     return child;
@@ -68,8 +109,65 @@ function addRule(){
     buildRuleElement();
 }
 
-elemRuleAdd.addEventListener("click", addRule);
+function setCurrentData(v){
+    currentData = v;
+    elemDataCurrent.value = v.join(',');
+}
 
+elemRuleAdd.addEventListener("click", addRule);
+elemDataDefault.addEventListener("input", () => {
+    const text = elemDataDefault.value;
+    defaultData = text.split(",");
+    setCurrentData(defaultData);
+})
+
+
+
+elemControlPlay.addEventListener("click", () => {
+    playing = true;
+}); elemControlPause.addEventListener("click", () => {
+    playing = false;
+}); elemControlPrevious.addEventListener("click", () => {
+    playing = false;
+    currentPos = 0;
+    currentData = defaultData;
+});
+
+
+function turingStep(){
+    const cell = '';
+    if (currentPos > -1 && currentPos < currentData.length){
+        cell = currentData[currentPos];
+    }
+    const key = cell + ";" + currentState;
+    const v = turingRules[key];
+    if (v != undefined){
+        currentData[currentPos] = v[0];
+        currentState = v[1];
+        currentPos += v[2];
+    }
+    console.log(currentData);
+}
+
+function cycle(){
+    if (playing && Date.now()>lastStep){
+        lastStep = Date.now() + stepCool;
+        turingStep();
+    }
+    requestAnimationFrame(cycle);
+}
+
+retrieveRules();
+requestAnimationFrame(cycle);
+
+/*
+
+
+#,1,0,0,1,1,#
+
+
+
+*/
 /*
 data = ['#',1,0,0,1,1,'#']
 
